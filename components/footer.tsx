40e79d2220683f8db16f7b74f84ee63e79562d1a
Brian <rpt2fx@virginia.edu>
2026-02-26 19:05:06 -0500

    added UVA CIO disclaimer to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,6 +11,9 @@ export function Footer() {
             rpt2fx@virginia.edu
           </a>
         </p>
+        <p className="text-xs mt-4 max-w-4xl mx-auto">
+          Although this organization has members who are University of Virginia students and may have University employees associated or engaged in its activities and affairs, the organization is not a part of or an agency of the University. It is a separate and independent organization, which is responsible for and manages its own activities and affairs. The University does not direct, supervise or control the organization and is not responsible for the organization's contracts, acts or omissions.
+        </p>
       </div>
     </footer>
   )
